f1c08b9fcd855b3ecb25b2961af943c353d51050
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-07-20 10:43:21 -0700

    working game no css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,141 @@
 //declare variables and arrays
 //var authors = ["ANGELOU", "AUSTEN", "BRONTE", "DOSTOYEVSKY", "JOYCE", "KAFKA", "ROWLING", "SHAKESPEARE"];
 var authors = ["BEE", "ANT", "DOG"];
+var chosenAuthor;
 var dashesArray = [];
-var dashes;
 var wins = 0;
-var lettersYes = [];
 var lettersNo = [];
-var count = 0;
+var foundDashesInAuthors = true;
+var numGuesses = 10;
+var foundInLettersNo = false;
+var foundLetterInAuthors = 0;
+var lookForDashes;
 
 
 
-// Randomly chooses an author from the authors array. This is the Computer's choice.
-// It has full author names in it, like AUSTEN, BRONTE, etc
-var chosenAuthor = authors[Math.floor(Math.random() * authors.length)];
 
-//push as many underscores as there are letters in the author name into the dashesArray
-for (var i = 0; i < chosenAuthor.length; i++){
-	dashesArray.push(" _ ");
-}
+	// Randomly chooses an author from the authors array. This is the Computer's choice.
+	// It has full author names in it, like AUSTEN, BRONTE, etc
+	chosenAuthor = authors[Math.floor(Math.random() * authors.length)];
+console.log(chosenAuthor);
+	//push as many underscores as there are letters in the author name into the dashesArray
+	for (var i = 0; i < chosenAuthor.length; i++){
+		dashesArray.push(" _ ");
+	}
 
-//join the array into a string separated by white space instead of commas
-//put it in an onload function because at this point the html tags don't exist yet
-window.onload = function() {
-	
-	underlines.innerHTML = dashesArray.join(" ");
-	
- }
+	//join the array into a string separated by white space instead of commas
+	//put it in an onload function because at this point the html tags don't exist yet
+	//so this won't run until after html loads
+	window.onload = function() {
+		
+		guessedRight.innerHTML = dashesArray.join(" ");
+		
+	}
 
 
 // function executes when onkeyup event fires
-// word event can be waffle
+// word event can be waffle, it's not a reserved word
 document.onkeyup = function(event) {
+console.log("in the onkeyup function");
+	if(numGuesses > 0 && foundDashesInAuthors == true){	
 
-	// Determines which key was pressed.
-	// Make it uppercase in order to compare it with author name
-    var playerLetter = event.key.toUpperCase();
+		// Determines which key was pressed.
+		// Make it uppercase in order to compare it with author name
+	    var playerLetter = event.key.toUpperCase();
+console.log(playerLetter);
+	    // Check in the chosen author's name for the guessed playerLetter
+	    for (var i = 0; i < chosenAuthor.length; i++){
 
-    // Check in the chosen author's name for the guessed playerLetter
-    for (var i = 0; i < chosenAuthor.length; i++){
 
-    	count++;
+	    	// if the playerLetter is in the author's name, display the letter in guessedRight ID
+		    if (chosenAuthor[i] === playerLetter){
 
-    	// if the playerLetter is in the author's name, display the letter in guessedRight ID
-	    if (chosenAuthor[i] === playerLetter){
-	    	console.log(chosenAuthor[i] + "  " + playerLetter);
+		    	//put found letter in its correct location
+		    	dashesArray[i] = playerLetter;
 
-	    	guessedRight.innerHTML = event.key;
+		    	//display guess
+		    	guessedRight.innerHTML = dashesArray.join(" ");
+console.log(dashesArray);
+		    	//check to see if word has been guessed
+		    	//if yes, reset the game, no, keep playing
+		    	lookForDashes = dashesArray.indexOf(" _ ");
+
+	    		if(lookForDashes === -1){
+	    			foundDashesInAuthors = false;
+	    		}
+
+				foundLetterInAuthors++;
+				console.log(foundDashesInAuthors + " " + foundLetterInAuthors);
+
+		    } // end if
+
+		} // end for loop
+
+
+	    // if foundLetterInAuthors === 0, then that letter wasn't found. 
+	    if (foundLetterInAuthors === 0){
+
+	    	console.log("letter not found" + lettersNo.length + " " + lettersNo);
+
+		    // display the letter in guessedWrong ID
+	    	for (var k = 0; k < lettersNo.length; k++){
+	    		//console.log("lettersNo[k] " + lettersNo[k] + "playerLetter " + playerLetter);
+
+	    		if (lettersNo[k] === playerLetter){
+	    			foundInLettersNo = true;
+	    			//console.log("letter is in lettersNo");
+	    		}	
+	    	}
+
+	    	if (foundInLettersNo == false){
+	    		lettersNo.push(playerLetter);
+	    		guessedWrong.innerHTML = lettersNo.join(" ");
+	    		guessesLeft.innerHTML = --numGuesses;
+	    	}
+	    	else {
+	    		foundInLettersNo = false;
+	    	}
+	    	
 	    }
-	    // else if not found at any array index, once for has looped through the whole word
-	    // then display the letter in guessedWrong ID
-	    // reset count to 0 for the next letter
-	    else if (count === chosenAuthor.length){
-	    	lettersNo.push(" " + playerLetter);
-	    	guessedWrong.innerHTML = lettersNo;
-	    	count = 0;
-	    }
+	    foundLetterInAuthors = 0;
+
 	}
+	// if the author is guessed
+	else if (foundDashesInAuthors == false){
+		// add to wins
+		winsTotal.innerHTML = ++wins;
+		// reset flags and variables
+		numGuesses = 10;
+		foundDashesInAuthors = true;
+		foundInLettersNo = false;
+		dashesArray = [];
+		lettersNo = [];
+		//reset game
+		chosenAuthor = authors[Math.floor(Math.random() * authors.length)];
+		for (var i = 0; i < chosenAuthor.length; i++){
+			dashesArray.push(" _ ");
+		}
+		guessedRight.innerHTML = dashesArray.join(" ");
+		guessedWrong.innerHTML = lettersNo;
+	}
+	//number of guesses is used up
+	else if (numGuesses === 0){
+		// reset flags and variables
+		numGuesses = 10;
+		foundDashesInAuthors = true;
+		foundInLettersNo = false;
+		dashesArray = [];
+		lettersNo = [];
+		//reset game
+		chosenAuthor = authors[Math.floor(Math.random() * authors.length)];
+		for (var i = 0; i < chosenAuthor.length; i++){
+			dashesArray.push(" _ ");
+		}
+		guessedRight.innerHTML = dashesArray.join(" ");
+		guessedWrong.innerHTML = lettersNo;
+	}
+
+	
 }
+
+
